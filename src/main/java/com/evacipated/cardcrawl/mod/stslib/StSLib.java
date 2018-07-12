9181218cbb5a52e21829e96e0e3cf7d06038d601
Kiooeht <kiooeht@gmail.com>
2018-07-11 21:11:40 -0700

    Read keywords from json file

--- src/main/java/com/evacipated/cardcrawl/mod/stslib/StSLib.java
+++ src/main/java/com/evacipated/cardcrawl/mod/stslib/StSLib.java
@@ -1,14 +1,21 @@
 package com.evacipated.cardcrawl.mod.stslib;
 
 import basemod.BaseMod;
+import basemod.interfaces.EditCardsSubscriber;
 import basemod.interfaces.EditKeywordsSubscriber;
 import basemod.interfaces.EditStringsSubscriber;
+import com.badlogic.gdx.Gdx;
 import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
+import com.google.gson.Gson;
+import com.megacrit.cardcrawl.localization.Keyword;
+
+import java.nio.charset.StandardCharsets;
 
 @SpireInitializer
 public class StSLib implements
         EditKeywordsSubscriber,
-        EditStringsSubscriber
+        EditStringsSubscriber,
+        EditCardsSubscriber
 {
     public static void initialize()
     {
@@ -18,11 +25,25 @@ public class StSLib implements
     @Override
     public void receiveEditKeywords()
     {
+        Gson gson = new Gson();
+        String json = Gdx.files.internal("localization/stslib/keywords.json").readString(String.valueOf(StandardCharsets.UTF_8));
+        Keyword[] keywords = gson.fromJson(json, Keyword[].class);
 
+        if (keywords != null) {
+            for (Keyword keyword : keywords) {
+                BaseMod.addKeyword(keyword.NAMES, keyword.DESCRIPTION);
+            }
+        }
     }
 
     @Override
     public void receiveEditStrings()
+    {
+
+    }
+
+    @Override
+    public void receiveEditCards()
     {
 
     }
