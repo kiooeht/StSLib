726d2678e75fef2d207eb8d73bcf7bd8729dd1a4
Kiooeht <kiooeht@gmail.com>
2018-07-29 00:09:53 -0700

    Make stunned respect artifact

--- src/main/java/com/evacipated/cardcrawl/mod/stslib/actions/common/StunMonsterAction.java
+++ src/main/java/com/evacipated/cardcrawl/mod/stslib/actions/common/StunMonsterAction.java
@@ -8,6 +8,7 @@ import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.monsters.EnemyMoveInfo;
+import com.megacrit.cardcrawl.powers.ArtifactPower;
 
 import java.lang.reflect.Field;
 
@@ -25,15 +26,17 @@ public class StunMonsterAction extends AbstractGameAction
     public void update()
     {
         if (duration == Settings.ACTION_DUR_FAST) {
-            AbstractDungeon.actionManager.addToTop(new ApplyPowerAction(target, source, new StunMonsterPower((AbstractMonster)target), 1));
-            try {
-                Field f = AbstractMonster.class.getDeclaredField("move");
-                f.setAccessible(true);
-                EnemyMoveInfo move = (EnemyMoveInfo) f.get(target);
-                move.intent = AbstractMonster.Intent.STUN;
-                ((AbstractMonster) target).createIntent();
-            } catch (IllegalAccessException | NoSuchFieldException e) {
-                e.printStackTrace();
+            AbstractDungeon.actionManager.addToTop(new ApplyPowerAction(target, source, new StunMonsterPower((AbstractMonster) target), 1));
+            if (!target.hasPower(ArtifactPower.POWER_ID)) {
+                try {
+                    Field f = AbstractMonster.class.getDeclaredField("move");
+                    f.setAccessible(true);
+                    EnemyMoveInfo move = (EnemyMoveInfo) f.get(target);
+                    move.intent = AbstractMonster.Intent.STUN;
+                    ((AbstractMonster) target).createIntent();
+                } catch (IllegalAccessException | NoSuchFieldException e) {
+                    e.printStackTrace();
+                }
             }
         }
         tickDuration();
