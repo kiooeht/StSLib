6c811dd0406466ebcf82a3229944f170a14397bc
Kiooeht <kiooeht@gmail.com>
2018-11-11 22:52:11 -0800

    OnCardDrawPower

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,6 @@
 ## Changelog ##
 #### dev ####
+* `OnCardDrawPower`
 
 #### v1.6.0 ####
 * Keywords
---
+++ src/main/java/com/evacipated/cardcrawl/mod/stslib/powers/interfaces/OnCardDrawPower.java
@@ -0,0 +1,8 @@
+package com.evacipated.cardcrawl.mod.stslib.powers.interfaces;
+
+import com.megacrit.cardcrawl.cards.AbstractCard;
+
+public interface OnCardDrawPower
+{
+    void onCardDraw(AbstractCard drawnCard);
+}
