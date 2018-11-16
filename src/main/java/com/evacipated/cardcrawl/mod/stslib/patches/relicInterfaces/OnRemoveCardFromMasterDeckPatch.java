69bd40a3f315c48e7292b928a29dcfea2ce325ec
Reina <ReinaSHSL@users.noreply.github.com>
2018-11-16 08:14:03 -0800

    OnRemoveCardFromMasterDeck relics

---
+++ src/main/java/com/evacipated/cardcrawl/mod/stslib/patches/relicInterfaces/OnRemoveCardFromMasterDeckPatch.java
@@ -0,0 +1,19 @@
+package com.evacipated.cardcrawl.mod.stslib.patches.relicInterfaces;
+
+import com.evacipated.cardcrawl.mod.stslib.relics.OnRemoveCardFromMasterDeckRelic;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.CardGroup;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
+
+@SpirePatch(clz = CardGroup.class, method = "removeCard", paramtypez = AbstractCard.class)
+public class OnRemoveCardFromMasterDeckPatch {
+    public static void Postfix(CardGroup __instance, AbstractCard c) {
+        for (AbstractRelic r : AbstractDungeon.player.relics) {
+            if (r instanceof OnRemoveCardFromMasterDeckRelic && __instance.type == CardGroup.CardGroupType.MASTER_DECK) {
+                ((OnRemoveCardFromMasterDeckRelic) r).onRemoveCardFromMasterDeck(c);
+            }
+        }
+    }
+}
---
+++ src/main/java/com/evacipated/cardcrawl/mod/stslib/relics/OnRemoveCardFromMasterDeckRelic.java
@@ -0,0 +1,7 @@
+package com.evacipated.cardcrawl.mod.stslib.relics;
+
+import com.megacrit.cardcrawl.cards.AbstractCard;
+
+public interface OnRemoveCardFromMasterDeckRelic {
+        void onRemoveCardFromMasterDeck(AbstractCard c);
+}
