d442af995a38feb5dd3dd0f9ac4d750c5ee126e5
Kiooeht <kiooeht@gmail.com>
2019-02-14 19:37:19 -0800

    Let powers change red/green color for amount2

--- src/main/java/com/evacipated/cardcrawl/mod/stslib/powers/abstracts/TwoAmountPower.java
+++ src/main/java/com/evacipated/cardcrawl/mod/stslib/powers/abstracts/TwoAmountPower.java
@@ -9,9 +9,9 @@ import com.megacrit.cardcrawl.powers.AbstractPower;
 public abstract class TwoAmountPower extends AbstractPower
 {
     public int amount2 = 0;
-    private Color redColor = Color.RED.cpy();
-    private Color greenColor = Color.GREEN.cpy();
     public boolean canGoNegative2 = false;
+    protected Color redColor2 = Color.RED.cpy();
+    protected Color greenColor2 = Color.GREEN.cpy();
 
     @Override
     public void renderAmount(SpriteBatch sb, float x, float y, Color c)
@@ -20,14 +20,14 @@ public abstract class TwoAmountPower extends AbstractPower
 
         if (amount2 > 0) {
             if (!isTurnBased) {
-                greenColor.a = c.a;
-                c = greenColor;
+                greenColor2.a = c.a;
+                c = greenColor2;
             }
             FontHelper.renderFontRightTopAligned(sb, FontHelper.powerAmountFont,
                     Integer.toString(amount2), x, y + 15 * Settings.scale, fontScale, c);
-            redColor.a = c.a;
-            c = redColor;
         } else if (amount2 < 0 && canGoNegative2) {
+            redColor2.a = c.a;
+            c = redColor2;
             FontHelper.renderFontRightTopAligned(sb, FontHelper.powerAmountFont,
                     Integer.toString(amount2), x, y + 15 * Settings.scale, fontScale, c);
         }
