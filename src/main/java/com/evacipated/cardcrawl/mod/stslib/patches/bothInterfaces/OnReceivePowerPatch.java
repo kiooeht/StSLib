2b7bdec48abc284c3c7eb74d844ed5ead3a8f92d
BDWSSBB <middlescatter@gmail.com>
2019-05-29 02:35:32 -0700

    Fixed OnReceivePowerPatch to be accustomed to beta structure.

--- src/main/java/com/evacipated/cardcrawl/mod/stslib/patches/bothInterfaces/OnReceivePowerPatch.java
+++ src/main/java/com/evacipated/cardcrawl/mod/stslib/patches/bothInterfaces/OnReceivePowerPatch.java
@@ -8,7 +8,6 @@ import com.evacipated.cardcrawl.modthespire.lib.*;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.utility.TextAboveCreatureAction;
-import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
@@ -18,47 +17,131 @@ import javassist.CtBehavior;
 
 public class OnReceivePowerPatch
 {
-    static SpireReturn<Void> CheckPower(AbstractGameAction action, AbstractCreature target, AbstractCreature source, float[] duration, AbstractPower powerToApply)
-    {
-        if (source != null) {
-            for (AbstractPower power : source.powers) {
-                if (power instanceof BetterOnApplyPowerPower) {
-                    // Allows changing the stackAmount
-                    action.amount = ((BetterOnApplyPowerPower) power).betterOnApplyPowerStacks(powerToApply, target, source, action.amount);
-                    // Allows negating the power
-                    boolean apply = ((BetterOnApplyPowerPower) power).betterOnApplyPower(powerToApply, target, source);
-                    if (!apply) {
-                        AbstractDungeon.actionManager.addToTop(new TextAboveCreatureAction(target, ApplyPowerAction.TEXT[0]));
-                        duration[0] -= Gdx.graphics.getDeltaTime();
-                        CardCrawlGame.sound.play("NULLIFY_SFX");
-                        return SpireReturn.Return(null);
-                    }
+
+    public static class OnReceivePowerPowerPatch {
+
+        @SpirePatch(
+                clz = ApplyPowerAction.class,
+                method = "update"
+        )
+        public static class ChangeStack {
+
+            @SpireInsertPatch(
+                    locator = Locator.class,
+                    localvars = {"p", "powerToApply"}
+            )
+            public static void Insert(ApplyPowerAction __instance, AbstractPower power, AbstractPower powerToApply) {
+                // Allow changing the stackAmount. We are not using changeAmount(), that will be deleted.
+                if (power instanceof OnReceivePowerPower) {
+                    __instance.amount = ((OnReceivePowerPower) power).onReceivePowerStacks(powerToApply, __instance.target, __instance.source, __instance.amount);
+                }
+            }
+
+            private static class Locator extends SpireInsertLocator {
+                @Override
+                public int[] Locate(CtBehavior ctMethodToPatch) throws Exception {
+                    // We are not using the onReceivingPowerStack() hook, that will be deleted.
+                    // Warning: onReceivingPower() hook name will change soon.
+                    Matcher finalMatcher = new Matcher.MethodCallMatcher(AbstractPower.class, "onReceivingPower");
+                    return LineFinder.findInOrder(ctMethodToPatch, finalMatcher);
                 }
             }
         }
 
-        if (target != null) {
-            for (AbstractPower power : target.powers) {
-                if (power instanceof OnReceivePowerPower) {
-                    // Allows changing the stackAmount
-                    action.amount = ((OnReceivePowerPower) power).onReceivePowerStacks(powerToApply, target, source, action.amount);
-                    // Allows negating the power
-                    boolean apply = ((OnReceivePowerPower) power).onReceivePower(powerToApply, target, source);
-                    if (!apply) {
-                        AbstractDungeon.actionManager.addToTop(new TextAboveCreatureAction(target, ApplyPowerAction.TEXT[0]));
-                        duration[0] -= Gdx.graphics.getDeltaTime();
-                        CardCrawlGame.sound.play("NULLIFY_SFX");
-                        return SpireReturn.Return(null);
+        // Warning: onReceivingPower() hook name will change soon.
+        @SpirePatch(
+                clz = AbstractPower.class,
+                method = "onReceivingPower"
+        )
+        public static class NegatePower {
+
+            public static boolean Postfix(AbstractPower __instance, AbstractPower power, AbstractCreature target, AbstractCreature source) {
+                if (__instance instanceof OnReceivePowerPower) {
+                    boolean apply = ((OnReceivePowerPower) __instance).onReceivePower(power, target, source);
+                    if (apply) {
+                        return true;
+                    }
+                    else {
+                        return false;
                     }
                 }
+                else {
+                    return true;
+                }
             }
-            if (target.isPlayer) {
-                for (AbstractRelic relic : AbstractDungeon.player.relics) {
-                    if (relic instanceof OnReceivePowerRelic) {
+        }
+    }
+
+    public static class OnReceivePowerRelicPatch {
+
+        @SpirePatch(
+                clz = ApplyPowerAction.class,
+                method = "update"
+        )
+        public static class ChangeStack {
+
+            @SpireInsertPatch(
+                    locator = Locator.class,
+                    localvars = {"r", "powerToApply"}
+            )
+            public static void Insert(ApplyPowerAction __instance, AbstractRelic relic, AbstractPower powerToApply) {
+                // Allow changing the stackAmount. We are not using changeAmount(), that will be deleted.
+                if (relic instanceof OnReceivePowerRelic) {
+                    __instance.amount = ((OnReceivePowerRelic) relic).onReceivePowerStacks(powerToApply, __instance.source, __instance.amount);
+                }
+            }
+
+            private static class Locator extends SpireInsertLocator {
+                @Override
+                public int[] Locate(CtBehavior ctMethodToPatch) throws Exception {
+                    // We are not using the onReceivingPowerStack() hook, that will be deleted.
+                    // Warning: onReceivingPower() hook name will change soon.
+                    Matcher finalMatcher = new Matcher.MethodCallMatcher(AbstractRelic.class, "onReceivingPower");
+                    return LineFinder.findInOrder(ctMethodToPatch, finalMatcher);
+                }
+            }
+        }
+
+        // Warning: onReceivingPower() hook name will change soon.
+        @SpirePatch(
+                clz = AbstractRelic.class,
+                method = "onReceivingPower"
+        )
+        public static class NegatePower {
+
+            public static boolean Postfix(AbstractRelic __instance, AbstractPower power, AbstractCreature target, AbstractCreature source) {
+                if (__instance instanceof OnReceivePowerRelic) {
+                    boolean apply = ((OnReceivePowerRelic) __instance).onReceivePower(power, source);
+                    if (apply) {
+                        return true;
+                    }
+                    else {
+                        return false;
+                    }
+                }
+                else {
+                    return true;
+                }
+            }
+        }
+    }
+
+    // We are not using the existing version in the base game, the related hooks will be deleted soon.
+    @SpirePatch(
+            clz=ApplyPowerAction.class,
+            method="update"
+    )
+    public static class OnReceivePowerBetterOnApplyPower {
+
+        static SpireReturn<Void> CheckPower(AbstractGameAction action, AbstractCreature target, AbstractCreature source, float[] duration, AbstractPower powerToApply)
+        {
+            if (source != null) {
+                for (AbstractPower power : source.powers) {
+                    if (power instanceof BetterOnApplyPowerPower) {
                         // Allows changing the stackAmount
-                        action.amount = ((OnReceivePowerRelic) relic).onReceivePowerStacks(powerToApply, source, action.amount);
+                        action.amount = ((BetterOnApplyPowerPower) power).betterOnApplyPowerStacks(powerToApply, target, source, action.amount);
                         // Allows negating the power
-                        boolean apply = ((OnReceivePowerRelic) relic).onReceivePower(powerToApply, source);
+                        boolean apply = ((BetterOnApplyPowerPower) power).betterOnApplyPower(powerToApply, target, source);
                         if (!apply) {
                             AbstractDungeon.actionManager.addToTop(new TextAboveCreatureAction(target, ApplyPowerAction.TEXT[0]));
                             duration[0] -= Gdx.graphics.getDeltaTime();
@@ -68,16 +151,9 @@ public class OnReceivePowerPatch
                     }
                 }
             }
+            return SpireReturn.Continue();
         }
-        return SpireReturn.Continue();
-    }
 
-    @SpirePatch(
-            clz=ApplyPowerAction.class,
-            method="update"
-    )
-    public static class ApplyPower
-    {
         @SpireInsertPatch(
                 locator = Locator.class,
                 localvars = {"duration", "powerToApply"}
@@ -92,7 +168,8 @@ public class OnReceivePowerPatch
             @Override
             public int[] Locate(CtBehavior ctMethodToPatch) throws Exception
             {
-                Matcher finalMatcher = new Matcher.MethodCallMatcher(AbstractPlayer.class, "hasRelic");
+                // Warning: This will definitely need a new matcher soon.
+                Matcher finalMatcher = new Matcher.FieldAccessMatcher(AbstractCreature.class, "powers");
                 return LineFinder.findInOrder(ctMethodToPatch, finalMatcher);
             }
         }
