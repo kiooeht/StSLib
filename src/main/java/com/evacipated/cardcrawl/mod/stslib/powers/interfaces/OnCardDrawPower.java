36e935bc99cf1a5a398d09fa41f9639e89a875b7
Kiooeht <kiooeht@gmail.com>
2019-07-06 15:46:22 -0700

    Fix onCardDraw() being called twice on powers
    Deprecated OnCardDrawPower

--- src/main/java/com/evacipated/cardcrawl/mod/stslib/powers/interfaces/OnCardDrawPower.java
+++ src/main/java/com/evacipated/cardcrawl/mod/stslib/powers/interfaces/OnCardDrawPower.java
@@ -2,6 +2,7 @@ package com.evacipated.cardcrawl.mod.stslib.powers.interfaces;
 
 import com.megacrit.cardcrawl.cards.AbstractCard;
 
+@Deprecated
 public interface OnCardDrawPower
 {
     void onCardDraw(AbstractCard drawnCard);
