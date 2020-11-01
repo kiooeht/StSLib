b1034764bb0457de8e203bb6618109fbc0b36a1d
erasels <emanuelbedburdick@yahoo.de>
2020-11-01 11:51:37 +0100

    Added BeforeIntentRender power interface

---
+++ src/main/java/com/evacipated/cardcrawl/mod/stslib/powers/interfaces/BeforeRenderIntentPower.java
@@ -0,0 +1,14 @@
+package com.evacipated.cardcrawl.mod.stslib.powers.interfaces;
+
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+public interface BeforeRenderIntentPower {
+    /**
+     * @param monster      The monster that the intent is about to be rendered for
+     * @return            Allows stopping the intent from rendering (false = don't render)
+     */
+    default boolean beforeRenderIntent(AbstractMonster monster)
+    {
+        return true;
+    }
+}
