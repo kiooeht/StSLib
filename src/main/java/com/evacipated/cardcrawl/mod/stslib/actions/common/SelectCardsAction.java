7f3788ac778845c09d9e12f84fcad19241360101
Vex Darkon <edanvirgo@gmail.com>
2021-01-08 22:14:33 -0700

    Use correct gridSelectScreen.open

--- src/main/java/com/evacipated/cardcrawl/mod/stslib/actions/common/SelectCardsAction.java
+++ src/main/java/com/evacipated/cardcrawl/mod/stslib/actions/common/SelectCardsAction.java
@@ -92,7 +92,7 @@ public class SelectCardsAction
                 return;
             }
 
-            AbstractDungeon.gridSelectScreen.open(selectGroup, amount, text, anyNumber);
+            AbstractDungeon.gridSelectScreen.open(selectGroup, amount, anyNumber, text);
             tickDuration();
         }
 
