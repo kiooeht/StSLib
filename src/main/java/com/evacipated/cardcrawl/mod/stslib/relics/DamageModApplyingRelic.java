d98ca4b982a7f115d1af6a85cf70f3ace792b460
Alison Turner <alisonmoons@gmail.com>
2022-01-26 18:08:44 -0400

    DamageModApplyingRelic and DamageModApplyingPower now have List return types instead of ArrayList. onAddDamageInfo now has default instead of requiring an additional override.

--- src/main/java/com/evacipated/cardcrawl/mod/stslib/relics/DamageModApplyingRelic.java
+++ src/main/java/com/evacipated/cardcrawl/mod/stslib/relics/DamageModApplyingRelic.java
@@ -3,14 +3,12 @@ package com.evacipated.cardcrawl.mod.stslib.relics;
 import com.evacipated.cardcrawl.mod.stslib.damagemods.AbstractDamageModifier;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public interface DamageModApplyingRelic {
-
-    void onAddedDamageModsToDamageInfo(DamageInfo info, Object instigator);
+    default void onAddedDamageModsToDamageInfo(DamageInfo info, Object instigator) {}
 
     boolean shouldPushMods(DamageInfo infoMayBeNull, Object instigator, List<AbstractDamageModifier> activeDamageModifiers);
 
-    ArrayList<AbstractDamageModifier> modsToPush(DamageInfo infoMayBeNull, Object instigator, List<AbstractDamageModifier> activeDamageModifiers);
+    List<AbstractDamageModifier> modsToPush(DamageInfo infoMayBeNull, Object instigator, List<AbstractDamageModifier> activeDamageModifiers);
 }
