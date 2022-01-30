e3b9e777e5c55679c6853e5104ef21bbae0c1131
Alison Turner <alisonmoons@gmail.com>
2022-01-30 02:30:45 -0400

    CustomIconHelper now longer has an arraylist and getAllIcons returns the values of the hashmap

--- src/main/java/com/evacipated/cardcrawl/mod/stslib/icons/CustomIconHelper.java
+++ src/main/java/com/evacipated/cardcrawl/mod/stslib/icons/CustomIconHelper.java
@@ -1,14 +1,12 @@
 package com.evacipated.cardcrawl.mod.stslib.icons;
 
-import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 
 public class CustomIconHelper {
-    private static final ArrayList<AbstractCustomIcon> allIcons = new ArrayList<>();
     private static final HashMap<String, AbstractCustomIcon> icons = new HashMap<>();
 
     public static void addCustomIcon(AbstractCustomIcon icon) {
-        allIcons.add(icon);
         icons.put(icon.cardCode().toLowerCase(), icon);
     }
 
@@ -16,7 +14,7 @@ public class CustomIconHelper {
         return icons.get(key.toLowerCase());
     }
 
-    public static ArrayList<AbstractCustomIcon> getAllIcons() {
-        return allIcons;
+    public static Collection<AbstractCustomIcon> getAllIcons() {
+        return icons.values();
     }
 }
