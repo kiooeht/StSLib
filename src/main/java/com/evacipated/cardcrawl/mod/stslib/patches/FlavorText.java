3bba00916367c483f32bd42bc336fd6f5038f791
Bryan <BryanMKerns@gmail.com>
2022-02-25 01:28:39 -0600

    Move a lot of common code in TipHelperRenderFlavorPatch and TipHelperRenderFlavorSCV into one function. I no longer spell flavor as flaver anywhere.

--- src/main/java/com/evacipated/cardcrawl/mod/stslib/patches/FlavorText.java
+++ src/main/java/com/evacipated/cardcrawl/mod/stslib/patches/FlavorText.java
@@ -41,78 +41,88 @@ public class FlavorText {
 
     public static AbstractCard card;
 
-    public static enum boxType {
+    public enum boxType {
         WHITE,
         TRADITIONAL,
         CUSTOM
+    }
+
+    private static float addFlavorText(float x, float y, SpriteBatch sb, AbstractCard card) {
+        if (card == null)
+            return y;
+
+        Color boxColor;
+        Color textColor;
+        String s;
+
+        s = AbstractCardFlavorFields.flavor.get(card);
+        boxColor = AbstractCardFlavorFields.boxColor.get(card);
+        textColor = AbstractCardFlavorFields.textColor.get(card);
+        if (boxColor == null || s == null || s.equals("") || textColor == null)
+            return y;
+
+        float BODY_TEXT_WIDTH = ReflectionHacks.getPrivate(null, TipHelper.class, "BODY_TEXT_WIDTH");
+        float TIP_DESC_LINE_SPACING = ReflectionHacks.getPrivate(null, TipHelper.class, "TIP_DESC_LINE_SPACING");
+        float SHADOW_DIST_X = ReflectionHacks.getPrivate(null, TipHelper.class, "SHADOW_DIST_X");
+        float SHADOW_DIST_Y = ReflectionHacks.getPrivate(null, TipHelper.class, "SHADOW_DIST_Y");
+        float BOX_EDGE_H = ReflectionHacks.getPrivate(null, TipHelper.class, "BOX_EDGE_H");
+        float BOX_BODY_H = ReflectionHacks.getPrivate(null, TipHelper.class, "BOX_BODY_H");
+        float BOX_W = ReflectionHacks.getPrivate(null, TipHelper.class, "BOX_W");
+        float TEXT_OFFSET_X = ReflectionHacks.getPrivate(null, TipHelper.class, "TEXT_OFFSET_X");
+
+        float h = -FontHelper.getSmartHeight(flavorFont, s, BODY_TEXT_WIDTH, TIP_DESC_LINE_SPACING)
+                - 40.0F * Settings.scale;
+
+        if (TIP_BOT == null || TIP_MID == null || TIP_TOP == null)
+            setTextures();
+
+        Texture topTexture = TIP_TOP;
+        Texture midTexture = TIP_MID;
+        Texture botTexture = TIP_BOT;
+
+        if (AbstractCardFlavorFields.flavorBoxType.get(card) == boxType.TRADITIONAL) {
+            topTexture = ImageMaster.KEYWORD_TOP;
+            midTexture = ImageMaster.KEYWORD_BODY;
+            botTexture = ImageMaster.KEYWORD_BOT;
+        }
+        else if (AbstractCardFlavorFields.flavorBoxType.get(card) == boxType.CUSTOM &&
+                AbstractCardFlavorFields.boxTop.get(card) != null &&
+                AbstractCardFlavorFields.boxMid.get(card) != null &&
+                AbstractCardFlavorFields.boxBot.get(card) != null)
+        {
+            topTexture = AbstractCardFlavorFields.boxTop.get(card);
+            midTexture = AbstractCardFlavorFields.boxMid.get(card);
+            botTexture = AbstractCardFlavorFields.boxBot.get(card);
+        }
+
+        sb.setColor(Settings.TOP_PANEL_SHADOW_COLOR);
+        sb.draw(ImageMaster.KEYWORD_TOP, x + SHADOW_DIST_X, y - SHADOW_DIST_Y, BOX_W, BOX_EDGE_H);
+        sb.draw(ImageMaster.KEYWORD_BODY, x + SHADOW_DIST_X, y - h - BOX_EDGE_H - SHADOW_DIST_Y,
+                BOX_W, h + BOX_EDGE_H);
+        sb.draw(ImageMaster.KEYWORD_BOT, x + SHADOW_DIST_X, y - h - BOX_BODY_H - SHADOW_DIST_Y,
+                BOX_W, BOX_EDGE_H);
+        sb.setColor(boxColor.cpy());
+        sb.draw(topTexture, x, y, BOX_W, BOX_EDGE_H);
+        sb.draw(midTexture, x, y - h - BOX_EDGE_H, BOX_W, h + BOX_EDGE_H);
+        sb.draw(botTexture, x, y - h - BOX_BODY_H, BOX_W, BOX_EDGE_H);
+
+        FontHelper.renderSmartText(sb, flavorFont, s,x + TEXT_OFFSET_X,
+                y + 13.0F * Settings.scale, BODY_TEXT_WIDTH, TIP_DESC_LINE_SPACING,
+                textColor);
+
+        y -= h + BOX_EDGE_H * 3.15F;
+
+        return y;
     }
 
     @SpirePatch2(
             clz = TipHelper.class,
             method = "renderKeywords"
     )
-    public static class TipHelperRenderFlaverPatch{
+    public static class TipHelperRenderFlavorPatch{
         @SpirePostfixPatch
-        public static void TipHelperRenderFlaver(float x, @ByRef float[] y, SpriteBatch sb,
-                                               ArrayList<String> keywords,
-                                               float ___TIP_DESC_LINE_SPACING, float ___BODY_TEXT_WIDTH,
-                                               float ___BOX_EDGE_H, float ___SHADOW_DIST_X, float ___SHADOW_DIST_Y,
-                                               float ___BOX_W, float ___BOX_BODY_H, float ___TEXT_OFFSET_X,
-                                               AbstractCard ___card) {
-            if (___card == null)
-                return;
-
-            Color boxColor;
-            Color textColor;
-            String s;
-
-            s = AbstractCardFlavorFields.flavor.get(___card);
-            boxColor = AbstractCardFlavorFields.boxColor.get(___card);
-            textColor = AbstractCardFlavorFields.textColor.get(___card);
-            if (boxColor == null || s == null || s.equals("") || textColor == null)
-                return;
-
-            float h = -FontHelper.getSmartHeight(flavorFont, s, ___BODY_TEXT_WIDTH, ___TIP_DESC_LINE_SPACING)
-                    - 40.0F * Settings.scale;
-
-            if (TIP_BOT == null || TIP_MID == null || TIP_TOP == null)
-                setTextures();
-
-            Texture topTexture = TIP_TOP;
-            Texture midTexture = TIP_MID;
-            Texture botTexture = TIP_BOT;
-
-            if (AbstractCardFlavorFields.flavorBoxType.get(___card) == boxType.TRADITIONAL) {
-                topTexture = ImageMaster.KEYWORD_TOP;
-                midTexture = ImageMaster.KEYWORD_BODY;
-                botTexture = ImageMaster.KEYWORD_BOT;
-            }
-            else if (AbstractCardFlavorFields.flavorBoxType.get(___card) == boxType.CUSTOM &&
-                    AbstractCardFlavorFields.boxTop.get(___card) != null &&
-                    AbstractCardFlavorFields.boxMid.get(___card) != null &&
-                    AbstractCardFlavorFields.boxBot.get(___card) != null)
-            {
-                topTexture = AbstractCardFlavorFields.boxTop.get(___card);
-                midTexture = AbstractCardFlavorFields.boxMid.get(___card);
-                botTexture = AbstractCardFlavorFields.boxBot.get(___card);
-            }
-
-            sb.setColor(Settings.TOP_PANEL_SHADOW_COLOR);
-            sb.draw(ImageMaster.KEYWORD_TOP, x + ___SHADOW_DIST_X, y[0] - ___SHADOW_DIST_Y, ___BOX_W, ___BOX_EDGE_H);
-            sb.draw(ImageMaster.KEYWORD_BODY, x + ___SHADOW_DIST_X, y[0] - h - ___BOX_EDGE_H - ___SHADOW_DIST_Y,
-                    ___BOX_W, h + ___BOX_EDGE_H);
-            sb.draw(ImageMaster.KEYWORD_BOT, x + ___SHADOW_DIST_X, y[0] - h - ___BOX_BODY_H - ___SHADOW_DIST_Y,
-                    ___BOX_W, ___BOX_EDGE_H);
-            sb.setColor(boxColor.cpy());
-            sb.draw(topTexture, x, y[0], ___BOX_W, ___BOX_EDGE_H);
-            sb.draw(midTexture, x, y[0] - h - ___BOX_EDGE_H, ___BOX_W, h + ___BOX_EDGE_H);
-            sb.draw(botTexture, x, y[0] - h - ___BOX_BODY_H, ___BOX_W, ___BOX_EDGE_H);
-
-            FontHelper.renderSmartText(sb, flavorFont, s,x + ___TEXT_OFFSET_X,
-                    y[0] + 13.0F * Settings.scale, ___BODY_TEXT_WIDTH, ___TIP_DESC_LINE_SPACING,
-                    textColor);
-
-            y[0] -= h + ___BOX_EDGE_H * 3.15F;
+        public static void TipHelperRenderFlavor(float x, @ByRef float[] y, SpriteBatch sb, AbstractCard ___card) {
+            y[0] = addFlavorText(x, y[0], sb, ___card);
         }
     }
 
@@ -201,64 +211,8 @@ public class FlavorText {
     )
     public static class TipHelperRenderFlavorSCV {
         @SpirePostfixPatch
-        public static void TipHelperRenderFlavor(float x, @ByRef float[] y, SpriteBatch sb,
-                                                 float ___TIP_DESC_LINE_SPACING, float ___BODY_TEXT_WIDTH,
-                                                 float ___BOX_EDGE_H, float ___SHADOW_DIST_X, float ___SHADOW_DIST_Y,
-                                                 float ___BOX_W, float ___BOX_BODY_H, float ___TEXT_OFFSET_X) {
-            if (card == null)
-                return;
-
-            Color boxColor;
-            Color textColor;
-            String s;
-
-            s = AbstractCardFlavorFields.flavor.get(card);
-            boxColor = AbstractCardFlavorFields.boxColor.get(card);
-            textColor = AbstractCardFlavorFields.textColor.get(card);
-            if (boxColor == null || s == null || s.equals("") || textColor == null)
-                return;
-
-            if (TIP_BOT == null || TIP_MID == null || TIP_TOP == null)
-                setTextures();
-
-            Texture topTexture = TIP_TOP;
-            Texture midTexture = TIP_MID;
-            Texture botTexture = TIP_BOT;
-
-            if (AbstractCardFlavorFields.flavorBoxType.get(card) == boxType.TRADITIONAL) {
-                topTexture = ImageMaster.KEYWORD_TOP;
-                midTexture = ImageMaster.KEYWORD_BODY;
-                botTexture = ImageMaster.KEYWORD_BOT;
-            }
-            else if (AbstractCardFlavorFields.flavorBoxType.get(card) == boxType.CUSTOM &&
-                    AbstractCardFlavorFields.boxTop.get(card) != null &&
-                    AbstractCardFlavorFields.boxMid.get(card) != null &&
-                    AbstractCardFlavorFields.boxBot.get(card) != null)
-            {
-                topTexture = AbstractCardFlavorFields.boxTop.get(card);
-                midTexture = AbstractCardFlavorFields.boxMid.get(card);
-                botTexture = AbstractCardFlavorFields.boxBot.get(card);
-            }
-
-            float h = -FontHelper.getSmartHeight(flavorFont, s, ___BODY_TEXT_WIDTH, ___TIP_DESC_LINE_SPACING)
-                    - 40.0F * Settings.scale;
-
-            sb.setColor(Settings.TOP_PANEL_SHADOW_COLOR);
-            sb.draw(ImageMaster.KEYWORD_TOP, x + ___SHADOW_DIST_X, y[0] - ___SHADOW_DIST_Y, ___BOX_W, ___BOX_EDGE_H);
-            sb.draw(ImageMaster.KEYWORD_BODY, x + ___SHADOW_DIST_X, y[0] - h - ___BOX_EDGE_H - ___SHADOW_DIST_Y,
-                    ___BOX_W, h + ___BOX_EDGE_H);
-            sb.draw(ImageMaster.KEYWORD_BOT, x + ___SHADOW_DIST_X, y[0] - h - ___BOX_BODY_H - ___SHADOW_DIST_Y,
-                    ___BOX_W, ___BOX_EDGE_H);
-            sb.setColor(boxColor.cpy());
-            sb.draw(topTexture, x, y[0], ___BOX_W, ___BOX_EDGE_H);
-            sb.draw(midTexture, x, y[0] - h - ___BOX_EDGE_H, ___BOX_W, h + ___BOX_EDGE_H);
-            sb.draw(botTexture, x, y[0] - h - ___BOX_BODY_H, ___BOX_W, ___BOX_EDGE_H);
-
-            FontHelper.renderSmartText(sb, flavorFont, s, x + ___TEXT_OFFSET_X,
-                    y[0] + 13.0F * Settings.scale, ___BODY_TEXT_WIDTH, ___TIP_DESC_LINE_SPACING,
-                    textColor);
-
-            y[0] -= h + ___BOX_EDGE_H * 3.15F;
+        public static void TipHelperRenderFlavor(float x, @ByRef float[] y, SpriteBatch sb) {
+            y[0] = addFlavorText(x, y[0], sb, card);
         }
     }
 
