a70d0f8e6686bb9009505503c5296105cc0a1b97
Alchyr <isithalchyr@gmail.com>
2022-04-20 10:22:38 -0700

    Multi-group card selection/move actions

---
+++ src/main/java/com/evacipated/cardcrawl/mod/stslib/actions/common/MultiGroupMoveAction.java
@@ -0,0 +1,118 @@
+package com.evacipated.cardcrawl.mod.stslib.actions.common;
+
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.CardGroup;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.UIStrings;
+
+import java.util.function.Predicate;
+
+public class MultiGroupMoveAction extends MultiGroupSelectAction {
+    private static final int EXHAUST_PILE_X, EXHAUST_PILE_Y;
+
+    static {
+        EXHAUST_PILE_X = (int) (Settings.WIDTH - 40.0F * Settings.scale);
+        EXHAUST_PILE_Y = (int) (200 * Settings.scale);
+    }
+
+    protected static final UIStrings uiStrings = CardCrawlGame.languagePack.getUIString("stslib:MoveCardsAction");
+    private static final String[] TEXT = uiStrings.TEXT;
+    public static final String HAND = uiStrings.TEXT_DICT.get("HAND");
+    public static final String DRAW_PILE = uiStrings.TEXT_DICT.get("DRAW");
+    public static final String DISCARD_PILE = uiStrings.TEXT_DICT.get("DISCARD");
+    public static final String EXHAUST_PILE = uiStrings.TEXT_DICT.get("EXHAUST");
+    public static final String UNKNOWN = "???";
+
+    public MultiGroupMoveAction(CardGroup.CardGroupType destination, int amount, CardGroup.CardGroupType... sources) {
+        this(destination, amount, false, (c)->true, sources);
+    }
+
+    public MultiGroupMoveAction(CardGroup.CardGroupType destination, int amount, boolean anyNumber, CardGroup.CardGroupType... sources) {
+        this(destination, amount, anyNumber, (c)->true, sources);
+    }
+
+    public MultiGroupMoveAction(CardGroup.CardGroupType destination, int amount, Predicate<AbstractCard> canSelect, CardGroup.CardGroupType... sources) {
+        this(destination, amount, false, canSelect, sources);
+    }
+
+    public MultiGroupMoveAction(CardGroup.CardGroupType destination, int amount, boolean anyNumber, Predicate<AbstractCard> canSelect, CardGroup.CardGroupType... sources) {
+        super(makeText(amount, destination), (cards, sourceMap)->{
+            for (AbstractCard c : AbstractDungeon.gridSelectScreen.selectedCards) {
+                moveToDestination(sourceMap.getOrDefault(c, AbstractDungeon.player.limbo), destination, c);
+            }
+        }, amount, anyNumber, canSelect, sources);
+    }
+
+    private static void moveToDestination(CardGroup source, CardGroup.CardGroupType destination, AbstractCard c) {
+        if (source.type == CardGroup.CardGroupType.EXHAUST_PILE)
+            c.unfadeOut();
+
+        switch (destination) {
+            case HAND:
+                source.moveToHand(c);
+                setCardPosition(source, c);
+                break;
+            case DRAW_PILE:
+                source.moveToDeck(c, true);
+                setCardPosition(source, c);
+                break;
+            case DISCARD_PILE:
+                source.moveToDiscardPile(c);
+                setCardPosition(source, c);
+                break;
+            case EXHAUST_PILE:
+                source.moveToExhaustPile(c);
+                setCardPosition(source, c);
+                break;
+            default:
+                System.out.println("MultiGroupMoveAction attempting to move to cardgroup of invalid type: " + destination.name());
+                break;
+        }
+    }
+
+    private static void setCardPosition(CardGroup source, AbstractCard c) {
+        switch (source.type) {
+            case DRAW_PILE:
+                c.current_x = CardGroup.DRAW_PILE_X;
+                c.current_y = CardGroup.DRAW_PILE_Y;
+                break;
+            case DISCARD_PILE:
+                c.current_x = CardGroup.DISCARD_PILE_X;
+                c.current_y = CardGroup.DISCARD_PILE_Y;
+                break;
+            case EXHAUST_PILE:
+                c.current_x = EXHAUST_PILE_X;
+                c.current_y = EXHAUST_PILE_Y;
+                break;
+        }
+        //Hand: Don't need to move. Anything else: what the heck
+    }
+
+    private static String makeText(int amount, CardGroup.CardGroupType destination) {
+        String ret;
+        if (amount == 1) {
+            ret = TEXT[0];
+        } else {
+            ret = TEXT[1];
+        }
+
+        String location = UNKNOWN;
+        switch (destination) {
+            case HAND:
+                location = HAND;
+                break;
+            case DRAW_PILE:
+                location = DRAW_PILE;
+                break;
+            case DISCARD_PILE:
+                location = DISCARD_PILE;
+                break;
+            case EXHAUST_PILE:
+                location = EXHAUST_PILE;
+                break;
+        }
+        return String.format(ret, location);
+    }
+}
---
+++ src/main/java/com/evacipated/cardcrawl/mod/stslib/patches/MultiGroupGridSelectPatches.java
@@ -0,0 +1,175 @@
+package com.evacipated.cardcrawl.mod.stslib.patches;
+
+import basemod.Pair;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.evacipated.cardcrawl.mod.stslib.actions.common.MultiGroupMoveAction;
+import com.evacipated.cardcrawl.modthespire.lib.*;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.CardGroup;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.helpers.FontHelper;
+import com.megacrit.cardcrawl.screens.select.GridCardSelectScreen;
+import javassist.CtBehavior;
+
+import java.util.ArrayList;
+
+public class MultiGroupGridSelectPatches {
+    private static final float TITLE_SPACING = 90 * Settings.scale;
+    private static final float TITLE_X = 300.0f * Settings.scale;
+    private static final float TITLE_Y = AbstractCard.IMG_HEIGHT * 0.75f / 2 + TITLE_SPACING / 2 + 20 * Settings.scale;
+
+    @SpirePatch(
+            clz = CardGroup.class,
+            method = SpirePatch.CLASS
+    )
+    public static class Fields {
+        public static SpireField<ArrayList<Pair<CardGroup.CardGroupType, Integer>>> groupIndexes = new SpireField<>(() -> null);
+    }
+
+    @SpirePatch(
+            clz = GridCardSelectScreen.class,
+            method = "updateCardPositionsAndHoverLogic"
+    )
+    public static class AlternatePositions {
+        @SpirePrefixPatch
+        public static SpireReturn<?> altPos(GridCardSelectScreen __instance, float ___drawStartX, float ___drawStartY, float ___currentDiffY, float ___padX, float ___padY, @ByRef AbstractCard[] ___hoveredCard) {
+            ArrayList<Pair<CardGroup.CardGroupType, Integer>> groupIndexes = Fields.groupIndexes.get(__instance.targetGroup);
+            DisplayGroupTitles.titles.clear();
+            if (groupIndexes != null) {
+                int lineNum = 0;
+                ArrayList<AbstractCard> cards = __instance.targetGroup.group;
+
+                int cardIndex = 0;
+                float groupOffset = 0;
+                for (Pair<CardGroup.CardGroupType, Integer> groupIndex : groupIndexes) {
+                    groupOffset += TITLE_SPACING;
+
+                    int firstGroupCardIndex = cardIndex;
+
+                    for (int i = 0; cardIndex < groupIndex.getValue(); ++cardIndex) {
+                        int mod = i % 5;
+                        if (mod == 0 && i != 0) {
+                            ++lineNum;
+                        }
+
+                        cards.get(cardIndex).target_x = ___drawStartX + (float)mod * ___padX;
+                        cards.get(cardIndex).target_y = ___drawStartY + ___currentDiffY - (float)lineNum * ___padY - groupOffset;
+                        cards.get(cardIndex).fadingOut = false;
+                        if (groupIndex.getKey() != CardGroup.CardGroupType.HAND)
+                            cards.get(cardIndex).update();
+                        cards.get(cardIndex).updateHoverLogic();
+
+                        ___hoveredCard[0] = null;
+
+                        for (AbstractCard c : cards) {
+                            if (c.hb.hovered) {
+                                ___hoveredCard[0] = c;
+                                break;
+                            }
+                        }
+
+                        ++i;
+                    }
+
+                    DisplayGroupTitles.titles.add(new GroupTitle(groupIndex.getKey(), TITLE_X, cards.get(firstGroupCardIndex).current_y + TITLE_Y));
+
+                    ++lineNum;
+                }
+
+                return SpireReturn.Return();
+            }
+
+            return SpireReturn.Continue();
+        }
+    }
+
+    @SpirePatch(
+            clz = GridCardSelectScreen.class,
+            method = "render"
+    )
+    public static class DisplayGroupTitles {
+        private static final ArrayList<GroupTitle> titles = new ArrayList<>();
+        private static final Color render = Color.WHITE.cpy();
+
+        @SpireInsertPatch(
+                locator = Locator.class
+        )
+        public static void renderTitles(GridCardSelectScreen __instance, SpriteBatch sb) {
+            for (GroupTitle title : titles) {
+                FontHelper.renderFontLeft(sb, FontHelper.losePowerFont, title.title, title.x, title.y, render);
+            }
+
+            titles.clear();
+        }
+
+        private static class Locator extends SpireInsertLocator {
+            public int[] Locate(CtBehavior ctBehavior) throws Exception {
+                Matcher finalMatcher = new Matcher.FieldAccessMatcher(GridCardSelectScreen.class, "hoveredCard");
+                return LineFinder.findInOrder(ctBehavior, finalMatcher);
+            }
+        }
+    }
+
+    private static class GroupTitle {
+        public String title;
+        public float x, y;
+
+        public GroupTitle(CardGroup.CardGroupType type, float x, float y){
+            switch (type) {
+                case HAND:
+                    title = MultiGroupMoveAction.HAND;
+                    break;
+                case DRAW_PILE:
+                    title = MultiGroupMoveAction.DRAW_PILE;
+                    break;
+                case DISCARD_PILE:
+                    title = MultiGroupMoveAction.DISCARD_PILE;
+                    break;
+                case EXHAUST_PILE:
+                    title = MultiGroupMoveAction.EXHAUST_PILE;
+                    break;
+                default:
+                    title = MultiGroupMoveAction.UNKNOWN;
+                    break;
+            }
+
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+
+    @SpirePatch(
+            clz = GridCardSelectScreen.class,
+            method = "calculateScrollBounds"
+    )
+    public static class AltScrollBounds {
+        @SpirePrefixPatch
+        public static SpireReturn<?> altPos(GridCardSelectScreen __instance, float ___padY, @ByRef float[] ___scrollUpperBound) {
+            ArrayList<Pair<CardGroup.CardGroupType, Integer>> groupIndexes = Fields.groupIndexes.get(__instance.targetGroup);
+            if (groupIndexes != null) {
+                int lineNum = -2;
+
+                int startIndex = 0;
+                for (Pair<CardGroup.CardGroupType, Integer> groupIndex : groupIndexes) {
+                    int size = groupIndex.getValue() - startIndex;
+
+                    startIndex = groupIndex.getValue();
+
+                    lineNum += size / 5;
+                    if (size % 5 != 0) {
+                        ++lineNum;
+                    }
+                }
+
+                float bonusScroll = (float)lineNum * ___padY + groupIndexes.size() * TITLE_SPACING;
+                ___scrollUpperBound[0] = Settings.DEFAULT_SCROLL_LIMIT + Math.max(0, bonusScroll);
+
+                return SpireReturn.Return();
+            }
+
+            return SpireReturn.Continue();
+        }
+    }
+}
