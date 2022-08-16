c896affc55c903ecc87e067c87304f16336c1176
Vex Darkon <edanvirgo@gmail.com>
2022-08-16 16:38:12 -0700

    Clarification

--- src/main/java/com/evacipated/cardcrawl/mod/stslib/cards/interfaces/UpgradeChangesPortraitCard.java
+++ src/main/java/com/evacipated/cardcrawl/mod/stslib/cards/interfaces/UpgradeChangesPortraitCard.java
@@ -1,4 +1,8 @@
 package com.evacipated.cardcrawl.mod.stslib.cards.interfaces;
 
 public interface UpgradeChangesPortraitCard {
+    /*
+    In addition to making your card implement this, you should call CustomCard loadCardImage on upgrade,
+    and override CustomCard getPortraitImage to return your alternate art if the card is upgraded.
+     */
 }
