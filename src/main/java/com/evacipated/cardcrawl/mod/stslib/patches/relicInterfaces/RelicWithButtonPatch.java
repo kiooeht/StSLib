db4cc6eb93c3dec0adbc0766f67e550b488b7931
Kiooeht <kiooeht@gmail.com>
2022-08-26 19:13:21 -0700

    Fix RelicWithButton not working on base game characters

--- src/main/java/com/evacipated/cardcrawl/mod/stslib/patches/relicInterfaces/RelicWithButtonPatch.java
+++ src/main/java/com/evacipated/cardcrawl/mod/stslib/patches/relicInterfaces/RelicWithButtonPatch.java
@@ -5,10 +5,18 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.evacipated.cardcrawl.mod.stslib.relics.ClickableForRelic;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch2;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
+import com.megacrit.cardcrawl.ui.panels.energyorb.EnergyOrbBlue;
+import com.megacrit.cardcrawl.ui.panels.energyorb.EnergyOrbGreen;
+import com.megacrit.cardcrawl.ui.panels.energyorb.EnergyOrbPurple;
+import com.megacrit.cardcrawl.ui.panels.energyorb.EnergyOrbRed;
 
 // Thanks to Alison for how to patch this in for proper update timing
 public class RelicWithButtonPatch
 {
+    @SpirePatch2(clz = EnergyOrbRed.class, method = "renderOrb")
+    @SpirePatch2(clz = EnergyOrbGreen.class, method = "renderOrb")
+    @SpirePatch2(clz = EnergyOrbBlue.class, method = "renderOrb")
+    @SpirePatch2(clz = EnergyOrbPurple.class, method = "renderOrb")
     @SpirePatch2(clz = CustomEnergyOrb.class, method = "renderOrb")
     public static class RenderElementAfterOrb
     {
@@ -21,6 +29,10 @@ public class RelicWithButtonPatch
         }
     }
 
+    @SpirePatch2(clz = EnergyOrbRed.class, method = "updateOrb")
+    @SpirePatch2(clz = EnergyOrbGreen.class, method = "updateOrb")
+    @SpirePatch2(clz = EnergyOrbBlue.class, method = "updateOrb")
+    @SpirePatch2(clz = EnergyOrbPurple.class, method = "updateOrb")
     @SpirePatch2(clz = CustomEnergyOrb.class, method = "updateOrb")
     public static class UpdateElementAfterOrb
     {
