42cbab420130e33ab0accfc86b29cef8b9d73301
Pandemonium14 <louislenne59@gmail.com>
2023-02-07 21:19:00 +0100

    Add shouldShowButton method to the interface, fixed scaling issues on non 1-scaled resolution

--- src/main/java/com/evacipated/cardcrawl/mod/stslib/relics/CardRewardSkipButton.java
+++ src/main/java/com/evacipated/cardcrawl/mod/stslib/relics/CardRewardSkipButton.java
@@ -5,6 +5,7 @@ import basemod.ReflectionHacks;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
@@ -15,10 +16,11 @@ import com.megacrit.cardcrawl.screens.CardRewardScreen;
 
 public class CardRewardSkipButton extends ClickableUIElement {
 
-    private static final float HITBOX_OFFSET_X = ImageMaster.REWARD_SCREEN_TAKE_BUTTON.getWidth() * 0.2f;
-    private static final float HITBOX_OFFSET_Y = ImageMaster.REWARD_SCREEN_TAKE_BUTTON.getHeight() * 0.3f;
+    private static final float HITBOX_OFFSET_X = ImageMaster.REWARD_SCREEN_TAKE_BUTTON.getWidth() * 0.2f * Settings.scale;
+    private static final float HITBOX_OFFSET_Y = ImageMaster.REWARD_SCREEN_TAKE_BUTTON.getHeight() * 0.3f * Settings.scale;
 
     private final CardRewardSkipButtonRelic relic;
+    private final TextureRegion texture;
 
     private final String label;
 
@@ -26,9 +28,10 @@ public class CardRewardSkipButton extends ClickableUIElement {
 
 
     public CardRewardSkipButton(CardRewardSkipButtonRelic relic) {
-        super(relic.getTexture(), 0, 0, ImageMaster.REWARD_SCREEN_TAKE_BUTTON.getWidth()*0.6f,ImageMaster.REWARD_SCREEN_TAKE_BUTTON.getHeight()*0.4f);
+        super(relic.getTexture(), 0, 0, ImageMaster.REWARD_SCREEN_TAKE_BUTTON.getWidth()*0.6f,ImageMaster.REWARD_SCREEN_TAKE_BUTTON.getHeight()*0.35f);
         this.relic = relic;
         label = relic.getButtonLabel();
+        texture = new TextureRegion(relic.getTexture());
         this.hitbox = new Hitbox(hb_w, hb_h);
 
     }
@@ -74,16 +77,16 @@ public class CardRewardSkipButton extends ClickableUIElement {
 
     public void render(SpriteBatch sb) {
         sb.setColor(Color.WHITE);
-        sb.draw(relic.getTexture(), x, y);
+        sb.draw(texture, x, y, texture.getRegionWidth()/2f * Settings.scale, texture.getRegionHeight()/2f * Settings.scale, texture.getRegionWidth()* Settings.scale, texture.getRegionHeight()* Settings.scale, 1f, 1f, 0f);
         if (lighten) {
             sb.setBlendFunction(770, 1);
             sb.setColor(new Color(1f,1f,1f,0.3f));
-            sb.draw(relic.getTexture(),x , y);
+            sb.draw(texture, x, y, texture.getRegionWidth()/2f * Settings.scale, texture.getRegionHeight()/2f * Settings.scale, texture.getRegionWidth()* Settings.scale, texture.getRegionHeight()* Settings.scale, 1f, 1f, 0f);
             sb.setBlendFunction(770, 771);
         }
 
-        float textX = x + relic.getTexture().getWidth()/2f;
-        float textY = y + relic.getTexture().getHeight()/2f;
+        float textX = x + texture.getRegionWidth()/2f * Settings.scale;
+        float textY = y + texture.getRegionHeight()/2f * Settings.scale;
         FontHelper.renderFontCentered(sb, FontHelper.buttonLabelFont, label, textX, textY, Color.WHITE, 1F);
         renderHitbox(sb);
     }
--- src/main/java/com/evacipated/cardcrawl/mod/stslib/relics/CardRewardSkipButtonRelic.java
+++ src/main/java/com/evacipated/cardcrawl/mod/stslib/relics/CardRewardSkipButtonRelic.java
@@ -1,13 +1,27 @@
 package com.evacipated.cardcrawl.mod.stslib.relics;
 
+import basemod.ReflectionHacks;
 import com.badlogic.gdx.graphics.Texture;
 import com.megacrit.cardcrawl.helpers.ImageMaster;
+import com.megacrit.cardcrawl.screens.CardRewardScreen;
 
 public interface CardRewardSkipButtonRelic {
 
     void onClickedButton();
 
     String getButtonLabel();
+
+    default boolean shouldShowButton(CardRewardScreen screen) {
+        return isActualCardReward(screen);
+    }
+
+    static boolean isActualCardReward(CardRewardScreen screen) {
+        boolean draft = ReflectionHacks.getPrivate(screen, CardRewardScreen.class, "draft");
+        boolean codex = ReflectionHacks.getPrivate(screen, CardRewardScreen.class, "codex");
+        boolean discovery = ReflectionHacks.getPrivate(screen, CardRewardScreen.class, "discovery");
+        boolean chooseOne = ReflectionHacks.getPrivate(screen, CardRewardScreen.class, "chooseOne");
+        return !draft && !codex && !discovery && !chooseOne;
+    }
 
     default Texture getTexture() {
         return ImageMaster.REWARD_SCREEN_TAKE_BUTTON;
