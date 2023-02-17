52bcf3a595a2e7938cf1dfa43c353b30f7fcc971
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2023-02-16 21:31:57 -0800

    blep

---
+++ src/main/java/com/evacipated/cardcrawl/mod/stslib/patches/SingleCardViewRenderPatch.java
@@ -0,0 +1,22 @@
+package com.evacipated.cardcrawl.mod.stslib.patches;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.evacipated.cardcrawl.mod.stslib.patches.cardInterfaces.MultiUpgradePatches;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch2;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.screens.SingleCardViewPopup;
+
+@SpirePatch2(
+        clz = SingleCardViewPopup.class,
+        method = "render"
+)
+public class SingleCardViewRenderPatch {
+
+    @SpirePostfixPatch
+    public static void postfixFix(SingleCardViewPopup __instance, SpriteBatch sb, AbstractCard ___card) {
+        ExtraIconsPatch.SingleCardViewRenderPatch.afterRender(__instance, sb);
+        MultiUpgradePatches.RenderTreeSCV.renderTree(__instance, sb, ___card);
+    }
+
+}
