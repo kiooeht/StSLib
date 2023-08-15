110f0522bede1548c5cb21ca57a4c441a92fee0e
Jedi515 <44167564+Jedi515@users.noreply.github.com>
2023-08-16 08:43:33 +1000

    Split create interface into 2
    One for "other create effects", one for "on create this card interface"

--- src/main/java/com/evacipated/cardcrawl/mod/stslib/StSLib.java
+++ src/main/java/com/evacipated/cardcrawl/mod/stslib/StSLib.java
@@ -10,6 +10,7 @@ import com.evacipated.cardcrawl.mod.stslib.cards.targeting.SelfOrEnemyTargeting;
 import com.evacipated.cardcrawl.mod.stslib.patches.CommonKeywordIconsPatches;
 import com.evacipated.cardcrawl.mod.stslib.patches.CustomTargeting;
 import com.evacipated.cardcrawl.mod.stslib.patches.bothInterfaces.OnCreateCardInterface;
+import com.evacipated.cardcrawl.mod.stslib.patches.bothInterfaces.OnCreateThisCardInterface;
 import com.evacipated.cardcrawl.mod.stslib.relics.ClickableForRelic;
 import com.evacipated.cardcrawl.mod.stslib.variables.ExhaustiveVariable;
 import com.evacipated.cardcrawl.mod.stslib.variables.PersistVariable;
@@ -215,8 +216,8 @@ public class StSLib implements
         AbstractDungeon.player.discardPile.group.stream().filter(card -> card instanceof OnCreateCardInterface).forEach(card -> ((OnCreateCardInterface) card).onCreateCard(c));
         AbstractDungeon.player.drawPile.group.stream().filter(card -> card instanceof OnCreateCardInterface).forEach(card -> ((OnCreateCardInterface) card).onCreateCard(c));
         AbstractDungeon.getMonsters().monsters.stream().filter(mon -> !mon.isDeadOrEscaped()).forEach(m -> m.powers.stream().filter(pow -> pow instanceof OnCreateCardInterface).forEach(pow -> ((OnCreateCardInterface) pow).onCreateCard(c)));
-        if (c instanceof OnCreateCardInterface) {
-            ((OnCreateCardInterface) c).onCreateThisCard();
+        if (c instanceof OnCreateThisCardInterface) {
+            ((OnCreateThisCardInterface) c).onCreateThisCard();
         }
         // Postfix here for custom hooks, I guess?
     }
--- src/main/java/com/evacipated/cardcrawl/mod/stslib/patches/bothInterfaces/OnCreateThisCardInterface.java
+++ src/main/java/com/evacipated/cardcrawl/mod/stslib/patches/bothInterfaces/OnCreateThisCardInterface.java
@@ -2,6 +2,6 @@ package com.evacipated.cardcrawl.mod.stslib.patches.bothInterfaces;
 
 import com.megacrit.cardcrawl.cards.AbstractCard;
 
-public interface OnCreateCardInterface {
-    void onCreateCard(AbstractCard card);
+public interface OnCreateThisCardInterface {
+    void onCreateThisCard();
 }
